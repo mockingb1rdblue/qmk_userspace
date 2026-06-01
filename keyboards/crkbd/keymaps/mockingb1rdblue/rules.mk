9c8b5a5853a47ddc309e556b75d4a82536654e78
mockingb1rdblue <115754285+mockingb1rdblue@users.noreply.github.com>
2026-06-01 16:30:24 -0600

    fix(keymaps): enable RGBLIGHT_ENABLE so RGB keycodes compile
    crkbd/rev1 and keebio/nyquist/rev3 keymaps reference RGB_TOG/RGB_MOD/
    RGB_HUI/RGB_SAI/RGB_VAI, which QMK only defines when RGBLIGHT_ENABLE=yes.
    Both boards have WS2812 underglow per their info.json, so enabling
    rgblight is the correct fix. reviung34 keymap uses no RGB keycodes and
    is unchanged.

--- keyboards/crkbd/keymaps/mockingb1rdblue/rules.mk
+++ keyboards/crkbd/keymaps/mockingb1rdblue/rules.mk
@@ -10,7 +10,7 @@ COMBO_ENABLE = yes
 LTO_ENABLE = yes # Enables Link Time Optimization (LTO) when compiling the keyboard. This makes the process take longer, but it can significantly reduce the compiled size (and since the firmware is small, the added time is not noticeable
 
 # CRKBD Specific
-RGBLIGHT_ENABLE = no
+RGBLIGHT_ENABLE = yes      # Required for RGB_TOG/RGB_MOD/RGB_HUI/RGB_SAI/RGB_VAI keycodes used in keymap
 RGB_MATRIX_ENABLE = no     # This one if there's LEDs. Enable WS2812 RGB underlight.
 OLED_ENABLE     = no
 # OLED_DRIVER_ENABLE = no  # OLD METHOD?
--- keyboards/keebio/nyquist/keymaps/mockingb1rdblue/rules.mk
+++ keyboards/keebio/nyquist/keymaps/mockingb1rdblue/rules.mk
@@ -8,3 +8,6 @@ VIA_ENABLE = yes
 # http://combos.gboards.ca/docs/install/
 COMBO_ENABLE=yes
 LTO_ENABLE = yes # Enables Link Time Optimization (LTO) when compiling the keyboard. This makes the process take longer, but it can significantly reduce the compiled size (and since the firmware is small, the added time is not noticeable
+
+# Nyquist rev3 has WS2812 RGB underglow; required for RGB_TOG/RGB_MOD/RGB_HUI/RGB_SAI/RGB_VAI keycodes used in keymap
+RGBLIGHT_ENABLE = yes
